7b96f37702a9ff727142863970f814121f78fdb4
Aung Min Oo <aungminoo1314@gmail.com>
2025-12-09 22:36:42 +0900

    only homepage left

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,46 @@
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobileMenu");
-const overlay = document.getElementById("overlay");
+//checkout selection
+document.querySelectorAll('input[name="payment"]').forEach((radio) => {
+	radio.addEventListener('change', function () {
 
-hamburger.addEventListener("click", () => {
-	// Toggle menu
-	mobileMenu.classList.toggle("open");
-	overlay.classList.toggle("show");
+		const cod = document.getElementById('cod-details');
+		const card = document.getElementById('card-details');
 
-	// Change icon
-	if (mobileMenu.classList.contains("open")) {
-		hamburger.setAttribute("icon", "mdi:close"); // X icon
-	} else {
-		hamburger.setAttribute("icon", "mdi:menu"); // Hamburger icon
-	}
+		cod.addEventListener("click", ()=>{console.log("hi")})
+
+		if (this.value === "cod") {
+			cod.style.display = "block";
+			card.style.display = "none";
+		} else {
+			cod.style.display = "none";
+			card.style.display = "block";
+		}
+	});
 });
 
-overlay.addEventListener("click", () => {
-	mobileMenu.classList.remove("open");
-	overlay.classList.remove("show");
+// const hamburger = document.getElementById("hamburger");
+// const mobileMenu = document.getElementById("mobileMenu");
+// const overlay = document.getElementById("overlay");
+//
+// hamburger.addEventListener("click", () => {
+// 	// Toggle menu
+// 	mobileMenu.classList.toggle("open");
+// 	overlay.classList.toggle("show");
+//
+// 	// Change icon
+// 	if (mobileMenu.classList.contains("open")) {
+// 		hamburger.setAttribute("icon", "mdi:close"); // X icon
+// 	} else {
+// 		hamburger.setAttribute("icon", "mdi:menu"); // Hamburger icon
+// 	}
+// });
+//
+// overlay.addEventListener("click", () => {
+// 	mobileMenu.classList.remove("open");
+// 	overlay.classList.remove("show");
+//
+// 	// Back to hamburger icon
+// 	hamburger.setAttribute("icon", "mdi:menu");
+// });
 
-	// Back to hamburger icon
-	hamburger.setAttribute("icon", "mdi:menu");
-});
+
+
