12611161fe8fa0711a1e3c2ec226ae342c2b486e
Aung Min Oo <aungminoo1314@gmail.com>
2025-12-13 20:17:35 +0900

    gg

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,8 +4,25 @@ import Shopping_Cart from "./pages/Shopping_Cart.jsx";
 import Checkout from "./pages/Checkout.jsx"
 import ProductDetail from "./pages/Product_Detail.jsx";
 import Receipt from "./pages/Receipt.jsx";
+import Loader from "./components/Loader.jsx";
+import {useEffect, useState} from "react";
 
 export default function App() {
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // simulate app boot time (fonts, images, state)
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 600); // adjust if needed
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) {
+        return <Loader />;
+    }
     return (
         <Routes>
             <Route path="/" element={<Home />} />
---
+++ react/src/components/Loader.jsx
@@ -0,0 +1,10 @@
+export default function Loader() {
+    return (
+        <div className="loader">
+            <div className="loader-box">
+                <div className="spinner" />
+                <p>読み込み中...</p>
+            </div>
+        </div>
+    );
+}
